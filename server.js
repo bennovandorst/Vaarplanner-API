80447678e3ff5f1720cdace8feba0006eb944df4
bennovandorst <9024828@student.zadkine.nl>
2024-10-13 03:19:37 +0200

    Hopefully fix CORS

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ import userRoutes from './routes/userRoutes.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: 'Content-Type'
+}));
 app.use(express.json());
 
 const PORT = process.env.PORT || 5005;
