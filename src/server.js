0e92cb26580c5a02c82ba2c51ca6c894a08f10e9
bennovandorst <9024828@student.zadkine.nl>
2024-10-20 20:06:06 +0200

    [🎉] Begin met avatar logic

--- src/server.js
+++ src/server.js
@@ -14,7 +14,8 @@ app.use(cors({
   methods: 'GET,POST,PUT,DELETE',
   allowedHeaders: 'Content-Type'
 }));
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 const PORT = process.env.PORT || 5005;
 
