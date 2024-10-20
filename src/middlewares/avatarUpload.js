220dfa7026d80ba1059f7255db64db5af729513c
bennovandorst <9024828@student.zadkine.nl>
2024-10-20 22:47:10 +0200

    0% PHP 100% Sigma

--- src/middlewares/avatarUpload.js
+++ src/middlewares/avatarUpload.js
@@ -4,7 +4,8 @@ import path from 'path';
 const storage = multer.diskStorage({
     destination: './uploads/avatars/',
     filename: (req, file, cb) => {
-        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+        const username = req.user.username;
+        cb(null, `avatar-${username}${path.extname(file.originalname)}`);
     }
 });
 
